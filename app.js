5afc0ed1008bac214193966ce0e66e5c01409425
Zeygon <luca.dev@outlook.com>
2017-04-22 14:23:54 +0200

    Added a bit of magic

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 L.marker([49.003008, 12.098255]).addTo(map)
     .bindPopup('<b>Your</b> Location')
     .openPopup();
+
+
+
+//Animations with jQuery
+var $searchOverlay = $('#search');
+var $searchTrigger = $('#fab');
+var $search = $('#input');
+
+
+$searchTrigger.click(function(e) {
+    $searchOverlay.fadeIn(500);
+    $search.focus();
+});
+
+$searchOverlay.find('.fa-times').click(function(e) {
+    $searchOverlay.fadeOut(500);
+});

0e87586667d4bfec0c11e96d91999ed9870bec8f
Hannes <johannes.ostner@beomuc.com>
2017-04-22 22:37:58 +0200

    added colors for tags and the correct creation of popups

--- app.js
+++ app.js
@@ -8,17 +8,37 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([49.003008, 12.098255], {icon: greenIcon}).addTo(map)
+L.marker([49.003008, 12.098255], { icon: greenIcon }).addTo(map)
     .bindPopup(samplePopup)
 
 $.getJSON('marker.json', function (data) {
-    for (var i = 0; i < data.marker.length; i++){
-        console.log(data.marker[i].name);
-        var p = '<h4 class="center" style="margin:0;">' + data.marker[i].name + '</h4><br><b>' + data.marker[i].opening + '</b><br>' + data.marker[i].description + '<br><p><span class="white-text badge red">#PARTY</span><span class="white-text badge blue">#SWAG</span></p><p style="margin-top:48px;font-weight:600;">' + data.marker[i].phone + ' &bull; ' + data.marker[i].url + ' &bull; ' + data.marker[i].email + '</p>';
-        L.marker(data.marker[i].coordinates, {icon: blackIcon}).addTo(map).bindPopup(p);
-
+    for (var i = 0; i < data.marker.length; i++) {
+        var marker_content = '<h4 class="center" style="margin:0;">' + data.marker[i].name + '</h4><br><b>' + data.marker[i].opening + '</b><br>' + data.marker[i].description + "<br><p>";
+        for (var tag_count = 0; tag_count < data.marker[i].tags.length; tag_count++) {
+            marker_content += '<span class="uppercase white-text badge ' + get_color(data.marker[i].tags[tag_count]) + '">';
+            marker_content += data.marker[i].tags[tag_count] + "</span>";
+        }
+        marker_content += '</p><p style="margin-top:48px;font-weight:600;"><hr><strong>Tel.:</strong>' + data.marker[i].phone + '<br><strong>Url:</strong>' + data.marker[i].url + '<br><strong>Email:</strong>' + data.marker[i].email + '</p>';
+        L.marker(data.marker[i].coordinates, { icon: blackIcon }).addTo(map).bindPopup(marker_content);
     }
 });
+
+function get_color(tag) {
+    switch (tag) {
+        case '#escalate':
+            return 'blue';
+        case '#party':
+            return 'red';
+        case '#music':
+            return 'yellow';
+        case '#action':
+            return 'green';
+        case '#sport':
+            return 'grey';
+        default:
+            return 'black';
+    }
+}
 
 
 //Animations with jQuery
@@ -48,6 +68,3 @@ $searchTrigger.click(function (e) {
 
 });
 
-
-
-var data = [];

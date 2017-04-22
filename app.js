4d7961a4a8959859f5975dac1b707ce8fe8b5271
Zeygon <luca.dev@outlook.com>
2017-04-22 23:21:31 +0200

    I'm an artist!

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([49.003008, 12.098255], {icon: greenIcon}).addTo(map)
+L.marker([49.003008, 12.098255], {
+        icon: greenIcon
+    }).addTo(map)
     .bindPopup(samplePopup)
 
-$.getJSON('marker.json', function (data) {
-    for (var i = 0; i < data.marker.length; i++){
+$.getJSON('marker.json', function(data) {
+    for (var i = 0; i < data.marker.length; i++) {
         console.log(data.marker[i].name);
         var p = '<h4 class="center" style="margin:0;">' + data.marker[i].name + '</h4><br><b>' + data.marker[i].opening + '</b><br>' + data.marker[i].description + '<br><p><span class="white-text badge red">#PARTY</span><span class="white-text badge blue">#SWAG</span></p><p style="margin-top:48px;font-weight:600;">' + data.marker[i].phone + ' &bull; ' + data.marker[i].url + ' &bull; ' + data.marker[i].email + '</p>';
-        L.marker(data.marker[i].coordinates, {icon: blackIcon}).addTo(map).bindPopup(p);
+        L.marker(data.marker[i].coordinates, {
+            icon: blackIcon
+        }).addTo(map).bindPopup(p);
 
     }
 });
@@ -28,17 +32,17 @@ var $search = $('#input');
 var $fabi = $('#fab--i');
 
 var b = false;
-$searchTrigger.click(function (e) {
+$searchTrigger.click(function(e) {
     if (b) {
         $searchOverlay.fadeOut(500);
-        setTimeout(function () { }, 500);
+        setTimeout(function() {}, 500);
         $fabi.text("search");
 
         b = false;
     } else {
 
         $searchOverlay.fadeIn(500);
-        setTimeout(function () {
+        setTimeout(function() {
             $search.focus();
         }, 500);
         $fabi.text("arrow_back");
@@ -50,4 +54,43 @@ $searchTrigger.click(function (e) {
 
 
 
-var data = [];
+
+
+var rangeSlider = function() {
+    var slider = $('#range--div'),
+        range = $('#range'),
+        value = $('#range--value');
+
+    slider.each(function() {
+
+        value.each(function() {
+            var value = $(this).prev().attr('value');
+            if (value === "23") {
+                $(this).html("HEUTE");
+
+            } else if (value === "24") {
+                $(this).html("MORGEN");
+
+            } else {
+
+                $(this).html(value + ". April");
+            }
+        });
+
+        range.on('input', function() {
+            if (this.value === "23") {
+                $(this).next(value).html("HEUTE");
+
+            } else if (this.value === "24") {
+                $(this).next(value).html("MORGEN");
+
+            } else {
+
+                $(this).next(value).html(this.value + ". April");
+            }
+
+        });
+    });
+};
+
+rangeSlider();
